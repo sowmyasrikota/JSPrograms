3e0396717005e264cbbdffd3f322a4c4aa6de43d
sowmya kota <venkatasowmyasrikota98@gmail.com>
2021-04-05 14:18:17 +0530

    arraysConcept

--- Js/arrays.js
+++ Js/arrays.js
@@ -22,24 +22,84 @@
 //     console.log("Hello "+names[i]);
 // }
 
-var names2 = ["oliver","kara","Barry","felicity"];
+// var names2 = ["oliver","kara","Barry","felicity"];
 
-var myObj = {
-    name: "sowmya",
-    course: "HTML/CSS/JAVASCRIPT",
-    platform: "coursera"
-};
+// var myObj = {
+//     name: "sowmya",
+//     course: "HTML/CSS/JAVASCRIPT",
+//     platform: "coursera"
+// };
 
-for(var prop in myObj){
-    console.log(myObj[prop]);
-}
+// for(var prop in myObj){
+//     console.log(myObj[prop]);
+// }
 
-for(var name in names2){
-    console.log(names2[name]);
-}
+// for(var name in names2){
+//     console.log(names2[name]);
+// }
 
-names2.greetings = "Hi!!";
+// names2.greetings = "Hi!!";
 
-for(var name in names2){
-    console.log(names2[name]);
-}
+// for(var name in names2){
+//     console.log(names2[name]);
+// }
+
+
+//arrays are infinite collection where space not defined remain undefined
+//forEach method
+
+var array = [1,2,3,4,];
+
+array.forEach(function(element){
+console.log(element*10);
+})
+
+//push
+//added at the end of array
+
+array.push(4);
+console.log(array);
+array.pop();
+console.log(array);
+array.shift();
+console.log(array);
+array.unshift('new');
+console.log(array);
+
+array[array.indexOf('new')] = 'old';
+console.log(array);
+
+//var newarray = array.splice(2,2);
+var newarray = array.slice(2,4);
+console.log(newarray);
+console.log(array);
+//array = [1,2,3,4];
+console.log(array.filter(function(value) {
+    return value > 2;
+}));
+
+console.log(array.map(function(value) {
+    return value*2;
+}));
+
+console.log(array.reverse());//changes the original one
+console.log(array);
+
+var newArray = ['join', 'me'];
+console.log(array.concat(newArray));
+console.log(array);
+console.log(newArray);
+console.log(array.join(newArray));
+console.log(array.join(', '));
+
+console.log(array.reduce(function(total,value){
+    console.log('Total: '+ total + ', Value: ' + value);
+    return total + value;
+}));
+
+
+
+
+
+
+
