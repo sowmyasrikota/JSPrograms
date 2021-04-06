bea61fe1a91fd7f04a69c8658d84c402d8d58ada
sowmya kota <venkatasowmyasrikota98@gmail.com>
2021-04-06 14:10:13 +0530

    changes in built in functions

--- Js/builtin_fun.js
+++ Js/builtin_fun.js
@@ -1,14 +1,84 @@
-var string = "Any text  ";
+// var string = "Any text  ";
 
-console.log(string.trim());
-console.log(string);
+// console.log(string.trim());
+// console.log(string);
 
-console.log(string.length);
+// console.log(string.length);
 
-console.log(string[2]);
+// console.log(string[2]);
 
-console.log(string.concat('add a string'));
+// console.log(string.concat('add a string'));
 
-console.log(string.toLocaleUpperCase());
+// console.log(string.toLocaleUpperCase());
 
-console.log(string.split(' '));
+// console.log(string.split(' '));
+
+// var pi = Math.PI;
+// console.log(pi);
+
+// var e = Math.E;
+// console.log(e);
+
+// var a = -3;
+// console.log(Math.abs(a));
+
+// a= 1.99;
+// console.log(Math.exp(a));
+
+// console.log(Math.log(e));
+
+// console.log(Math.max(1,10,100));
+
+// console.log(Math.random());
+
+// var rnd = Math.floor(Math.random()*100) +1;
+
+// console.log(rnd)
+
+
+// var today = new Date(2016,8,26);
+
+
+// var today1 = new Date('2016/05/26');
+// console.log(today.toString());
+
+// console.log(today1.toString());
+
+// console.log(Date.parse('2016/05/20'));
+
+// var thisday = new Date();
+
+// console.log(thisday.getDay());
+
+var string = 'abc';
+var pattern = /ab/;
+var pattern1 = /ac/;
+var string1 = 'abab';
+console.log(pattern);
+console.log(pattern.exec(string));
+console.log(string1.match(pattern));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
